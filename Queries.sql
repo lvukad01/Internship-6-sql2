1a6fa170d3676e531a0d51aadc6e6a01cafbb37d
lvukad01 <lana.vukadin1234@gmail.com>
2025-12-12 22:28:53 +0100

    Queries 4,5 and 6

--- Queries.sql
+++ Queries.sql
@@ -19,3 +19,38 @@ FROM Players p
 WHERE p.team_id=900;
 
 --4. Prikaži sve utakmice određenog turnira
+
+SELECT m.Match_datetime AS Date_and_time, t1.name AS team1, t2.name AS team2, m.type, CONCAT(m.team1_score, '-', m.team2_score) AS result
+FROM Matches m
+JOIN teams t1 ON t1.team_id=m.team1_id
+JOIN teams t2 ON t2.team_id=m.team2_id
+WHERE m.tournament_id=30;
+
+--5. Prikaži sve utakmice određenog tima kroz sve turnire
+
+SELECT 
+    m.match_datetime,tr.name AS tournament,
+	m.type AS match_type,
+	t1.name AS team1,
+    t2.name AS team2,
+    m.team1_score,
+    m.team2_score
+FROM Matches m
+JOIN Tournaments tr ON m.tournament_id = tr.tournament_id
+JOIN Teams t1 ON m.team1_id = t1.team_id
+JOIN Teams t2 ON m.team2_id = t2.team_id
+WHERE 10 IN (m.team1_id, m.team2_id)
+ORDER BY tr.name;
+
+--6. Izlistati sve događaje (golovi, kartoni) za određenu utakmicu
+--Prikazati tip događaja, ime igrača koji ga je ostvario.
+
+SELECT t.name AS tournament, m.type AS match_type, e.Event_type, p.name AS player 
+FROM events e
+JOIN matches m ON m.match_id=e.match_id
+JOIN tournaments t ON t.tournament_id=m.tournament_id
+JOIN players p ON p.player_id =e.player_id
+WHERE m.match_id=273;
+
+
+--
